fc4da6bde9c93b777643eb8b75b83752547d7824
eukamorph <evansb854@gmail.com>
2014-03-25 21:01:52 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
 $(document).ready(function () {
-    $("#envix").click(function () {
-        $(".zer0fuxc .futuresensec .kayimboc").hide();
-        $(".envixc").show();
-    });
-    $("#kayimbo").click(function () {
-        $(".envixc .futuresensec .zer0fuxc").hide();
-        $(".kayimboc").show();
-    });
-    $("#futuresense").click(function () {
-        $(".zer0fuxc .envixc .kayimboc").hide();
-        $(".futuresensec").show();
-    });
-    $("#all").click(function () {
-        $(".zer0fuxc .futuresensec .kayimboc .envixc").show();
-    });
-    
     $("#jellyfishart").click(function () {
         $("#envixarts").hide();
         $("#jellyfisharts").show();
@@ -24,6 +8,6 @@ $(document).ready(function () {
         $("#envixarts").show();
     });
     $("#allart").click(function () {
-        $("#envixarts #jellyfisharts").show();
+        $("#art").show();
     });
 });
