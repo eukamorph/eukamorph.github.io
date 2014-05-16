b062930fe480293f311e050acaa7ee2d54080914
eukamorph <evansb854@gmail.com>
2014-05-16 14:24:16 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,35 +2,39 @@
 $(document).ready(function () {
     "use strict";
     $("#asqd").click(function () {
-        $(".envixc, .kayimboc, .zer0fuxc, .futuresensec, .quantum77zc, .sketchartistc").hide();
+        $(".envixc, .kayimboc, .zer0fuxc, .futuresensec, .quantum77zc, .sketchartistc, .remzic").hide();
         $(".asqdc").show();
     });
     $("#envix").click(function () {
-        $(".asqdc, .kayimboc, .zer0fuxc, .futuresensec, .quantum77zc, .sketchartistc").hide();
+        $(".asqdc, .kayimboc, .zer0fuxc, .futuresensec, .quantum77zc, .sketchartistc, .remzic").hide();
         $(".envixc").show();
     });
     $("#futuresense").click(function () {
-        $(".asqdc, .kayimboc, .zer0fuxc, .envixc, .quantum77zc, .sketchartistc").hide();
+        $(".asqdc, .kayimboc, .zer0fuxc, .envixc, .quantum77zc, .sketchartistc, .remzic").hide();
         $(".futuresensec").show();
     });
     $("#kayimbo").click(function () {
-        $(".asqdc, .envixc, .zer0fuxc, .futuresensec, .quantum77zc, .sketchartistc").hide();
+        $(".asqdc, .envixc, .zer0fuxc, .futuresensec, .quantum77zc, .sketchartistc, .remzic").hide();
         $(".kayimboc").show();
     });
     $("#quantum77z").click(function () {
-        $(".asqdc, .kayimboc, .envixc, .zer0fuxc, .futuresensec, .sketchartistc").hide();
+        $(".asqdc, .kayimboc, .envixc, .zer0fuxc, .futuresensec, .sketchartistc, .remzic").hide();
         $(".quantum77zc").show();
     });
+    $("#remzi").click(function () {
+        $(".asqdc, .kayimboc, .envixc, .zer0fuxc, .futuresensec, .sketchartistc, .quantum77zc").hide();
+        $(".remzic").show();
+    });
     $("#sketchartist").click(function () {
-        $(".asqdc, .kayimboc, .envixc, .zer0fuxc, .futuresensec, .quantum77zc").hide();
+        $(".asqdc, .kayimboc, .envixc, .zer0fuxc, .futuresensec, .quantum77zc, .remzic").hide();
         $(".sketchartistc").show();
     });
     $("#zer0fux").click(function () {
-        $(".asqdc, .kayimboc, .envixc, .futuresensec, .quantum77zc, .sketchartistc").hide();
+        $(".asqdc, .kayimboc, .envixc, .futuresensec, .quantum77zc, .sketchartistc, .remzic").hide();
         $(".zer0fuxc").show();
     });
     $("#all").click(function () {
-        $(".asqdc, .envixc, .zer0fuxc, .kayimboc, .futuresensec, .quantum77zc, .sketchartistc, .zer0fuxc").show();
+        $(".asqdc, .envixc, .zer0fuxc, .kayimboc, .futuresensec, .quantum77zc, .sketchartistc, .zer0fuxc, .remzic").show();
     });
     
     $("#jellyfishart").click(function () {
